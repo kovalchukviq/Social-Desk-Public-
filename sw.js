c6a38e13675d0e51cac81bcbe7d4cb83b166641c
kovalchukviq <kovalchuk.viq@gmail.com>
2026-09-05 22:49:53 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE = 'social-desk-v12-1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-180.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/maskable-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  if (req.mode === 'navigate') {
+    e.respondWith(
+      fetch(req)
+        .then(r => { const cp = r.clone(); caches.open(CACHE).then(c => c.put('./index.html', cp)); return r; })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(req).then(hit => {
+      if (hit) return hit;
+      return fetch(req).then(r => {
+        if (r.ok && new URL(req.url, location.href).origin === location.origin) {
+          const cp = r.clone();
+          caches.open(CACHE).then(c => c.put(req, cp));
+        }
+        return r;
+      });
+    })
+  );
+});
